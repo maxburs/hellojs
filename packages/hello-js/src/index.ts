05f0956f603ec12dc2a2d3fbdcfe0905245301e8
Maxwell Burson <maxwellburson@gmail.com>
2026-04-02 22:41:24 -0700

    Fix type checking

--- packages/hello-js/src/index.ts
+++ packages/hello-js/src/index.ts
@@ -7,27 +7,27 @@ export interface HelloJSNode<
 > {
   $$node: typeof nodeIdentifier;
   tagName: T;
-  properties: HTMLElementDeprecatedTagNameMap[T];
-  children: readonly Node[];
+  properties: Partial<HTMLElementTagNameMap[T]>;
+  children: readonly HelloJSNode[];
 }
 
 export type HelloJSChild = null | false | string | HelloJSNode;
 
 export function h<T extends keyof HTMLElementTagNameMap>(
   tagName: T,
-  props?: HelloJSChild | HTMLElementTagNameMap[T],
+  props?: HelloJSChild | Partial<HTMLElementTagNameMap[T]>,
   ...children: HelloJSChild[]
 ): HelloJSNode<T> {
-  let properties: HTMLElementDeprecatedTagNameMap[T];
-  const nodes: readonly Node[] = [];
+  let properties: undefined | Partial<HTMLElementTagNameMap[T]>;
+  const nodes: HelloJSNode[] = [];
   let text = '';
 
   switch (typeof props) {
     case 'object':
-      if ('$$node' in props && props.$$node === nodeIdentifier) {
+      if (props && '$$node' in props && props.$$node === nodeIdentifier) {
         children.unshift(props);
       } else {
-        properties = props;
+        properties = props as Partial<HTMLElementTagNameMap[T]>;
       }
       break;
     case 'boolean':
@@ -51,12 +51,10 @@ export function h<T extends keyof HTMLElementTagNameMap>(
   }
 
   if (text) {
-    if (!properties) {
-      properties = { innerText: text };
-    } else {
-      properties = { innerText: '', ...properties };
+      properties = { innerText: '', ...properties }  as Partial<HTMLElementTagNameMap[T]>;
       properties.innerText += text;
-    }
+  } else {
+    properties ??= {};
   }
 
   return { $$node: nodeIdentifier, tagName, properties, children: nodes };
